7aef354aad9537a6205d78cbfcd88ffdd75e308b
ashraf magdy <ashrafmagdy926@gmail.com>
2025-11-27 17:48:21 +0200

    added todo list

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (!taskText) {
+      window.alert(" task is empty , please enter a task");
+    } else {
+      const li = document.createElement("li");
+      li.textContent = taskText;
+      const button = document.createElement("button");
+      button.classList.add("remove-btn");
+      button.textContent = "Remove";
+      button.onclick = function () {
+        taskList.removeChild(li);
+      };
+      li.appendChild(button);
+      taskList.appendChild(li);
+    }
+  }
+  addButton.addEventListener("click", addTask);
+  taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addTask();
+    }
+  });
+});
